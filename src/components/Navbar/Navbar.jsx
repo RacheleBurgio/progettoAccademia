0c07094200a023f39ac5db82d7613f3d0e629cb3
RacheleBurgio <rachele.burgio32@gmail.com>
2025-05-25 11:47:18 +0200

    aggiustata navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,97 +2,73 @@ import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
 import { Link } from 'react-router-dom'
 import styles from './Navbarcss.module.css'
-import Button from 'react-bootstrap/Button'
 import Dropdown from 'react-bootstrap/Dropdown'
+import Offcanvas from 'react-bootstrap/Offcanvas'
+import Container from 'react-bootstrap/Container'
 
 const MyNav = () => {
   return (
-    <Navbar className={styles.topNav} expand="lg">
-      <Navbar.Brand as={Link} to="/" style={{ marginLeft: '15px' }}>
-        <img className={styles.logonav} src="/ros-41.png" alt="Logo" />
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav">
-        <span className={styles.navbarTogglerIcon} />
-      </Navbar.Toggle>
-      <Navbar.Collapse id="basic-navbar-nav" className={styles.mobileCenter}>
-        <Nav className="me-auto">
-          <Dropdown as={Nav.Item}>
-            <Dropdown.Toggle as={Nav.Link} className="text-white fs-5">
-              ACADEMY
-            </Dropdown.Toggle>
-            {/* <Dropdown.Menu
-              className={`bg-dark text-white ${styles.dropdownMenuCenter}`}
-            >
-              <Dropdown.Item as={Link} to="/chisiamo">
-                Chi siamo
-              </Dropdown.Item>
-              <Dropdown.Item as={Link} to="/nostricorsi">
-                I nostri corsi
-              </Dropdown.Item>
-              <Dropdown.Item as={Link} to="/spettacolieventi">
-                Spettacoli ed eventi
-              </Dropdown.Item>
-            </Dropdown.Menu> */}
-          </Dropdown>
+    <Navbar expand="lg" className={styles.topNav}>
+      <Container fluid>
+        <Navbar.Brand as={Link} to="/" style={{ marginLeft: '15px' }}>
+          <img className={styles.logonav} src="/ros-41.png" alt="Logo" />
+        </Navbar.Brand>
 
-          <Dropdown as={Nav.Item}>
-            <Dropdown.Toggle as={Nav.Link} className="text-white fs-5">
-              PRODUCTIONS
-            </Dropdown.Toggle>
-            <Dropdown.Menu className={`${styles.dropdownMenuCenter}`}>
-              {/* <Dropdown.Item as={Link} to="/etichetta">
-                Etichetta
-              </Dropdown.Item> */}
-              <Dropdown.Item
-                className=" fs-5"
-                as={Link}
-                to="/ArtistiInPortafoglio"
-              >
-                Artisti in Portafoglio
-              </Dropdown.Item>
-              {/* <Dropdown.Item as={Link} to="/studiodiregistrazione">
-                Studio di registrazione
-              </Dropdown.Item> */}
-            </Dropdown.Menu>
-          </Dropdown>
+        {/* Toggle per mobile */}
+        <Navbar.Toggle
+          aria-controls="basic-navbar-nav"
+          className="border-0 custom-toggler"
+        >
+          <span className="navbar-toggler-icon" />
+        </Navbar.Toggle>
 
-          <Dropdown as={Nav.Item}>
-            <Dropdown.Toggle as={Nav.Link} className="text-white fs-5">
-              PARTNERSHIPS
-            </Dropdown.Toggle>
-            {/* <Dropdown.Menu className={` ${styles.dropdownMenuCenter}`}>
-              <Dropdown.Item as={Link} to="/casasanremolivebox">
-                Casa Sanremo live box
-              </Dropdown.Item>
-              <Dropdown.Item as={Link} to="/maestri">
-                Maestri
-              </Dropdown.Item>
-              <Dropdown.Item as={Link} to="/artisti">
-                Artisti
-              </Dropdown.Item>
-              <Dropdown.Item as={Link} to="/giornalisti">
-                Giornalisti
-              </Dropdown.Item>
-              <Dropdown.Item as={Link} to="/festivalofsong">
-                Giornalisti Festival od Song USA
-              </Dropdown.Item>
-            </Dropdown.Menu> */}
-          </Dropdown>
+        {/* Offcanvas che si apre da sinistra su mobile */}
+        <Navbar.Offcanvas
+          id="offcanvas-navbar"
+          aria-labelledby="offcanvas-navbar-label"
+          placement="start"
+          className={`${styles.mobileMenu} text-white`}
+        >
+          <Offcanvas.Header closeButton closeVariant="white">
+            <Offcanvas.Title id="offcanvas-navbar-label">Menu</Offcanvas.Title>
+          </Offcanvas.Header>
 
-          <Nav.Link href="#masterclass" className="text-white fs-5">
-            MASTERCLASS
-          </Nav.Link>
+          <Offcanvas.Body>
+            <Nav className="me-auto">
+              <Dropdown as={Nav.Item}>
+                <Dropdown.Toggle as={Nav.Link} className=" fs-5">
+                  ACADEMY
+                </Dropdown.Toggle>
+              </Dropdown>
 
-          {/* <Button
-            as={Link}
-            to="/login"
-            className={styles.bottone}
-            variant="outline-info"
-          >
-            Area Riservata
-          </Button> */}
-        </Nav>
-      </Navbar.Collapse>
+              <Dropdown as={Nav.Item}>
+                <Dropdown.Toggle as={Nav.Link} className=" fs-5">
+                  PRODUCTIONS
+                </Dropdown.Toggle>
+                <Dropdown.Menu className={`${styles.dropdownMenuCenter}`}>
+                  <Dropdown.Item
+                    className="fs-5"
+                    as={Link}
+                    to="/ArtistiInPortafoglio"
+                  >
+                    Artisti in Portafoglio
+                  </Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+
+              <Dropdown as={Nav.Item}>
+                <Dropdown.Toggle as={Nav.Link} className=" fs-5">
+                  PARTNERSHIPS
+                </Dropdown.Toggle>
+              </Dropdown>
+
+              <Nav.Link href="#masterclass" className=" fs-5">
+                MASTERCLASS
+              </Nav.Link>
+            </Nav>
+          </Offcanvas.Body>
+        </Navbar.Offcanvas>
+      </Container>
     </Navbar>
   )
 }
